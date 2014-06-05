5efa2050ac3aba3eae5082666f69559b336c7b59
Jack Lindamood <jack@signalfuse.com>
2014-06-04 20:19:05 -0700

    Basic javascript start

--- load_decks.js
+++ load_decks.js
@@ -1,6 +1,26 @@
-$.getJSON("http://localhost/index_blackjack/basic_strategy_6d_s17.json", function( data ) {
+$.getJSON("./basic_strategy_6d_s17.json", function( data ) {
   console.log(data);
+  $("#strategy_name").text(data.strategy);
   $("#number_of_decks").text(data.decks);
+  $("#double_after_split").text(data.DaS ? "Yes" : "No");
+  $("#surrender").text(data.surrender);
+  $("#hit_soft_17").text(data.hit_soft_17 ? "Yes": "No");
+  $("#hard_table").empty();
+  var head = $('<tr><td></td></tr>');
+  for (i=2;i<11;i++) {
+    head.append("<td>" + i + "</td>");
+  }
+  head.append("<td>A</td>");
+  $("#hard_table").append(head);
+
+  for (i=5;i<22;i++) {
+    head = $("<tr><td>" + i + "</td></tr>");
+    for (j=0;j<10;j++) {
+        head.append("<td>" + data.hard[i-5][j] + "</td>");
+    }
+    $("#hard_table").append(head);
+  }
+
 }).fail(function(){
   console.log("not valid json???");
 });
