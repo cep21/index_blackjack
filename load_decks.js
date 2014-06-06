8975fa924c1d46a53d56b6373c0cb1e40e5bc85b
Jack <jack@signalfuse.com>
2014-06-05 17:04:04 -0700

    Small changes

--- load_decks.js
+++ load_decks.js
@@ -5,25 +5,60 @@ $.getJSON("./basic_strategy_6d_s17.json",function (data) {
     $("#double_after_split").text(data.DaS ? "Yes" : "No");
     $("#surrender").text(data.surrender);
     $("#hit_soft_17").text(data.hit_soft_17 ? "Yes" : "No");
-    var hard_table = $("#hard_table")
-    hard_table.empty();
-    var head = $('<tr><td></td></tr>');
-    for (i = 2; i < 11; i++) {
-        head.append("<td>" + i + "</td>");
-    }
-    head.append("<td>A</td>");
-    hard_table.append(head);
-
-    for (i = 12; i < 22; i++) {
-        head = $("<tr><td>" + i + "</td></tr>");
-        for (j = 0; j < 10; j++) {
-            head.append("<td>" + data.hard[i - 12][j] + "</td>");
+    var card_tables = [null, "A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "A"];
+    {
+        var hard_table = $("#hard_table")
+        hard_table.empty();
+        var head = $('<tr><td></td></tr>');
+        for (i = 2; i < 12; i++) {
+            head.append("<td>" + card_tables[i] + "</td>");
         }
         hard_table.append(head);
+
+        for (i = 12; i < 22; i++) {
+            head = $("<tr><td>" + i + "</td></tr>");
+            for (j = 0; j < 10; j++) {
+                head.append("<td>" + data.hard[i - 12][j] + "</td>");
+            }
+            hard_table.append(head);
+        }
     }
 
-    var soft_table = $("soft_table")
-    soft_table.empty();
+    {
+        var soft_table = $("#soft_table")
+        soft_table.empty();
+        head = $('<tr><td></td></tr>');
+        for (i = 2; i < 12; i++) {
+            head.append("<td>" + card_tables[i] + "</td>");
+        }
+        soft_table.append(head);
+
+        for (i = 2; i < 11; i++) {
+            head = $("<tr><td> A/" + card_tables[i] + " (" + (11 + i) + ")</td></tr>");
+            for (j = 0; j < 10; j++) {
+                head.append("<td>" + data.soft[i - 2][j] + "</td>");
+            }
+            soft_table.append(head);
+        }
+    }
+    {
+        var hard_double = $("#hard_double_table")
+        hard_double.empty();
+        head = $('<tr><td></td></tr>');
+        for (i = 2; i < 12; i++) {
+            head.append("<td>" + card_tables[i] + "</td>");
+        }
+        hard_double.append(head);
+
+        for (i = 5; i < 12; i++) {
+            head = $("<tr><td>" + i + "</td></tr>");
+            for (j = 0; j < 10; j++) {
+                head.append("<td>" + data.hard_double[i - 5][j] + "</td>");
+            }
+            hard_double.append(head);
+        }
+    }
+
 
 }).fail(function () {
         console.log("not valid json???");
