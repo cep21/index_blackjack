8ea2c4d9ea1d0a134868c00a51a6b4e4cf2f088a
Jack <jack@signalfuse.com>
2014-07-07 15:47:38 -0700

    With help msg

--- startserver.py
+++ startserver.py
@@ -20,6 +20,7 @@ server_address = ('127.0.0.1', port)
 HandlerClass.protocol_version = Protocol
 httpd = ServerClass(server_address, HandlerClass)
 
+print "Visit http://%s:%d" % ('127.0.0.1', port)
 sa = httpd.socket.getsockname()
 # print "Serving HTTP on", sa[0], "port", sa[1], "..."
 httpd.serve_forever()
